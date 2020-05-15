d1846e4c27d8ef812180e0a0919f69f0fb168aff
Daniel Gipson <dannygipson95@gmail.com>
2020-05-15 13:54:38 -0400

    finished rendering characters and added portions of search

--- Answers.md
+++ Answers.md
@@ -2,8 +2,19 @@
 
 1. What is React JS and what problems does it solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
+* React is a javascript library that allows for quick and easy changes to the ui of a website based on data and user input
+
+
 1. Describe component state.
+
+* component state decides how a component renders and its behavior. A change in state changes the component's qualities
+
 
 1. Describe props.
 
+* props are the properties that are passed as an object to react components to be utilized
+
+
 1. What are side effects, and how do you sync effects in a React component to changes of certain state or props?
+
+* side effects are the result of a function or component interacting with and changing exterior environments. The second argument of useEffect allows us to determine what changes trigger the effect.
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,42 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    display: flex;
+    width: 40%;
+    margin: 5% 0;
+    padding: 1%;
+    border: 2px solid black;
+    border-radius: 5px;
+    background: #44CC14;
+
+    img {
+        width:50%;
+        border: 2px solid #80D91E;
+        border-radius: 10px;
+    }
+
+    `
+const CenteredDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+`
+
+export default function Character(props) {
+    const {name, species, status, image} = props;
+
+    return (
+        <StyledDiv>
+            <img src={image} alt={name}/>
+            <CenteredDiv>
+                <h2>{name}</h2>
+                <h4>Species: {species}</h4>
+                <h4>Status: {status}</h4>
+            </CenteredDiv>
+        </StyledDiv>
+    )
+}
